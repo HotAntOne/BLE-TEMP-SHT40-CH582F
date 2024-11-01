68162b2abc7bb444b36f3b5718ac4ecfef60846b
HotAntOne <1728141469@qq.com>
2024-11-01 22:56:06 +0800

    维护

--- BLE/BLE-TEMP-SHT40-CH582F/obj/RVMSIS/core_riscv.d
+++
@@ -1,2 +0,0 @@
-RVMSIS/core_riscv.o: \
- E:/WCH_Files/BLE-TEMP-SHT40-CH582F/SRC/RVMSIS/core_riscv.c
--- BLE/BLE-TEMP-SHT40-CH582F/obj/Startup/startup_CH583.d
+++
@@ -1,2 +0,0 @@
-Startup/startup_CH583.o: \
- E:/WCH_Files/BLE-TEMP-SHT40-CH582F/SRC/Startup/startup_CH583.S
